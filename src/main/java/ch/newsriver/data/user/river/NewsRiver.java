e44dc6c5f718fcff4e031478820374b5e9c63f82
Elia Palme <elia.palme@newscron.com>
2016-07-18 19:00:13 +0200

    defined river object

--- src/main/java/ch/newsriver/data/user/river/NewsRiver.java
+++ src/main/java/ch/newsriver/data/user/river/NewsRiver.java
@@ -1,7 +1,70 @@
 package ch.newsriver.data.user.river;
 
+import java.util.List;
+
 /**
  * Created by eliapalme on 17/07/16.
  */
 public class NewsRiver extends RiverBase {
+
+    private String name;
+    private List<Param> params;
+    private long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Param> getParams() {
+        return params;
+    }
+
+    public void setParams(List<Param> params) {
+        this.params = params;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public static class Param {
+        private String name;
+        private String condition;
+        private String value;
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCondition() {
+            return condition;
+        }
+
+        public void setCondition(String condition) {
+            this.condition = condition;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
 }
+
