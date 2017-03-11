c9e1ed918fca8600d553cd398231fe1f73284e98
Elia Palme <elia.palme@newscron.com>
2017-03-11 10:09:20 +0100

    added role to user object

--- src/main/java/ch/newsriver/data/user/UserFactory.java
+++ src/main/java/ch/newsriver/data/user/UserFactory.java
@@ -1,15 +1,15 @@
 package ch.newsriver.data.user;
 
 import ch.newsriver.dao.JDBCPoolUtil;
-import ch.newsriver.data.user.User;
-import ch.newsriver.data.user.river.RiverBase;
 import ch.newsriver.data.user.token.TokenFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by eliapalme on 17/07/16.
@@ -19,33 +19,56 @@ public class UserFactory {
     private static final Logger log = LogManager.getLogger(TokenFactory.class);
     private static ObjectMapper mapper = new ObjectMapper();
 
+    private static UserFactory instance;
 
-    public User getUser(long userId){
 
-        String sql = "SELECT U.id,U.name,U.email,R.id,R.value FROM user AS U LEFT JOIN riverSetting as R ON R.userId=U.id WHERE id = ?";
+    private UserFactory() {
+
+    }
+
+    static public synchronized UserFactory getInstance() {
+
+        if (instance == null) {
+            instance = new UserFactory();
+        }
+        return instance;
+    }
+
+    public User getUser(long userId) {
+
+        //Old version including saved rivers
+        // String sql = "SELECT U.id,U.name,U.email,U.role,R.id,R.value FROM user AS U LEFT JOIN riverSetting as R ON R.userId=U.id WHERE U.id = ?";
+
+        String sql = "SELECT U.id,U.name,U.email,U.role FROM user AS U WHERE U.id = ?";
         User user = new User();
-        try (Connection conn = JDBCPoolUtil.getInstance().getConnection(JDBCPoolUtil.DATABASES.Sources); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+        try (Connection conn = JDBCPoolUtil.getInstance().getConnection(JDBCPoolUtil.DATABASES.Sources); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setLong(1,userId);
+            stmt.setLong(1, userId);
             try (ResultSet resultSet = stmt.executeQuery()) {
 
                 if (resultSet.next()) {
                     user.setEmail(resultSet.getString("U.email"));
                     user.setName(resultSet.getString("U.name"));
-                    do{
-                        try{
-                            RiverBase river = mapper.readValue(resultSet.getString("R.value"),RiverBase.class);
-                            user.getRivers().put(resultSet.getLong("R.id"),river);
-                        }catch (IOException e){
-                            log.fatal("Unable to deserialize river",e);
+                    user.setRole(User.Role.valueOf(resultSet.getString("U.role")));
+                    /* Old version including saved rivers
+                    do {
+                        try {
+                            String riverStr = resultSet.getString("R.value");
+                            if(!resultSet.wasNull()) {
+                                RiverBase river = mapper.readValue(riverStr, RiverBase.class);
+                                user.getRivers().put(resultSet.getLong("R.id"), river);
+                            }
+                        } catch (IOException e) {
+                            log.fatal("Unable to deserialize river", e);
                         }
 
-                    }while (resultSet.next());
+                    } while (resultSet.next());
+                    */
                 }
             }
 
-        }catch (SQLException e){
-            log.fatal("Unable to generate token",e);
+        } catch (SQLException e) {
+            log.fatal("Unable to retreive user", e);
             return null;
         }
 
