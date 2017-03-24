dbaa0caa3435382a9e5d00abbeaa1230184febce
Elia Palme <elia.palme@newscron.com>
2017-03-24 08:46:03 +0100

    removed depth as its not used in source, eventually consider to add maxDepth

--- src/main/java/ch/newsriver/data/website/source/URLSeedSource.java
+++ src/main/java/ch/newsriver/data/website/source/URLSeedSource.java
@@ -13,9 +13,6 @@ public class URLSeedSource extends BaseSource {
     //Is this a root seed URL or temporary used to traverse the website.
     private boolean permanent;
 
-    //distance from the root seed URL
-    private int depth;
-
     private String countryName;
     private String countryCode;
     private String region;
@@ -31,15 +28,7 @@ public class URLSeedSource extends BaseSource {
     public void setPermanent(boolean permanent) {
         this.permanent = permanent;
     }
-
-    public int getDepth() {
-        return depth;
-    }
-
-    public void setDepth(int depth) {
-        this.depth = depth;
-    }
-
+    
     public String getCountryName() {
         return countryName;
     }
